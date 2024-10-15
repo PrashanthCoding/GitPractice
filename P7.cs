23d8fdd1ec43407fc62caf839447d598ac4adbe5
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-15 15:01:02 +0530

    P7.cs added

--- P7.cs
+++ P7.cs
@@ -4,9 +4,13 @@ class Program
 {
     static void Main()
     {
+
+        // Enter First Number
         Console.Write("Enter first number: ");
         int a = Convert.ToInt32(Console.ReadLine());
 
+
+        // Enter Second Number
         Console.Write("Enter second number: ");
         int b = Convert.ToInt32(Console.ReadLine());
 
