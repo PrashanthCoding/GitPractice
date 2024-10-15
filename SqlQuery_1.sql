07fc335808aad7545eabb504b83cafa862f4a576
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-15 17:25:45 +0530

    Update SqlQuery_1.sql

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,23 +1,19 @@
 ﻿INSERT INTO Customers (CustomerName, City, Country)
 VALUES ('Cardinal', 'Stavanger', 'Norway');
 
-
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
 VALUES
 ('Cardinal', 'Tom B. Erichsen', 'Skagen 21', 'Stavanger', '4006', 'Norway'),
 ('Greasy Burger', 'Per Olsen', 'Gateveien 15', 'Sandnes', '4306', 'Norway'),
 ('Tasty Tee', 'Finn Egan', 'Streetroad 19B', 'Liverpool', 'L1 0AA', 'UK');
 
-
 SELECT CustomerName, ContactName, Address
 FROM Customers
 WHERE Address IS NULL;
 
-
 SELECT CustomerName, ContactName, Address
 FROM Customers
 WHERE Address IS NOT NULL;
-
 
 UPDATE Customers
 SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
