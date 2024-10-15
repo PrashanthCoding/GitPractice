16108ceaa23202b2ba8ce3d51bbc723917d500c0
PrashanthCoding <prashanth100076@gmail.com>
2024-10-15 20:28:46 +0530

    textfile added

--- Program1.cs
+++ Program1.cs
@@ -3,7 +3,7 @@ using System;
 class Program
 {
     static void Main()
-    {
+    {                                                           
         Console.Write("Enter a number: ");
         int number = int.Parse(Console.ReadLine());
         int sum = 0, temp = number, digits = number.ToString().Length;
