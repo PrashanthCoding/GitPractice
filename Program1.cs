70a684cedc8f5da1865eabc2e498bcfb76c1cba3
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-15 15:30:32 +0530

    Program.cs changed

--- Program1.cs
+++ Program1.cs
@@ -5,7 +5,18 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("This is Prashanth");
-        Console.ReadLine();
+
+
+        Console.Write("Enter first number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter third number: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+        int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        Console.WriteLine("Largest number is: " + largest);
     }
 }
