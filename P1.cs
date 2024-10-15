96d41b7bb28a7bfb544ab37e88987dfea2c6bbaf
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-15 14:53:15 +0530

    Update P1.cs

--- P1.cs
+++ P1.cs
@@ -1,9 +1,22 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        int num1 = 0, num2 = 1, num3, i, n;
+
+        Console.Write("Enter the number of elements: ");
+        n = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write($"{num1} {num2} "); // printing first two elements
+
+        for (i = 2; i < n; ++i) // loop starts from 2
+        {
+            num3 = num1 + num2;
+            Console.Write(num3 + " ");
+            num1 = num2;
+            num2 = num3;
+        }
     }
 }
