4cd433877661db89320517d72a4b980268ef61c5
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-15 15:54:51 +0530

    Program1.cs added

--- Program1.cs
+++ Program1.cs
@@ -4,19 +4,27 @@ class Program
 {
     static void Main()
     {
-        int num1 = 0, num2 = 1, num3, i, n;
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter the number of elements: ");
-        n = Convert.ToInt32(Console.ReadLine());
+        bool isPrime = true;
 
-        Console.Write($"{num1} {num2} "); // printing first two elements
-
-        for (i = 2; i < n; ++i) // loop starts from 2
+        for (int i = 2; i <= num / 2; i++)
         {
-            num3 = num1 + num2;
-            Console.Write(num3 + " ");
-            num1 = num2;
-            num2 = num3;
+            if (num % i == 0)
+            {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+        {
+            Console.WriteLine(num + " is a prime number.");
+        }
+        else
+        {
+            Console.WriteLine(num + " is not a prime number.");
         }
     }
 }
