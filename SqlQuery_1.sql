ff04e388b83b0b14edbecb2d76d5ee4627335976
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-15 17:20:18 +0530

    Update SqlQuery_1.sql

--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,9 +1,11 @@
 ﻿SELECT * FROM Customers
 WHERE NOT Country = 'Australia';
 
-
 SELECT * FROM Customers
 WHERE CustomerID NOT BETWEEN 10 AND 60;
 
 SELECT * FROM Customers
 WHERE City NOT IN ('Paris', 'London');
+
+SELECT * FROM Customers
+WHERE NOT CustomerID > 50;
