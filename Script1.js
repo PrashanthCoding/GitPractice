1729a48c8c057e24d7de1e389b7f95caa78c5144
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-15 17:36:34 +0530

    Update Script1.js

--- Script1.js
+++ Script1.js
@@ -9,3 +9,12 @@ if (hour < 18) {
 } else {
     greeting = "Good evening";
 }
+
+
+if (time < 10) {
+  greeting = "Good morning";
+} else if (time < 20) {
+  greeting = "Good day";
+} else {
+  greeting = "Good evening";
+}
