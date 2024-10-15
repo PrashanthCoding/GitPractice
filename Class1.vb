73954754f138704bf76c7d4345f99bc0d26a4b44
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-15 17:01:39 +0530

    Class.vb added

--- Class1.vb
+++ Class1.vb
@@ -1,5 +1,21 @@
-﻿Imports Microsoft.VisualBasic
+﻿Imports System
 
-Public Class Class1
+Module Module1
+    Sub Main()
+        Dim arr() As Integer = {10, 20, 4, 45, 99}
 
-End Class
+        Dim largest As Integer = Integer.MinValue
+        Dim secondLargest As Integer = Integer.MinValue
+
+        For Each num As Integer In arr
+            If num > largest Then
+                secondLargest = largest
+                largest = num
+            ElseIf num > secondLargest AndAlso num <> largest Then
+                secondLargest = num
+            End If
+        Next
+
+        Console.WriteLine("The second largest element is " & secondLargest)
+    End Sub
+End Module
