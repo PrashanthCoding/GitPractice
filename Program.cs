f73f37d4d94d97a821dfe337ee0751312492b852
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-15 14:43:27 +0530

    Program.cs added

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+
+namespace MasterDataServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationMasterController : ControllerBase
+    {
+        private readonly MasterDataContext _context;
+
+        public LocationMasterController(MasterDataContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/locationmaster
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] LocationMaster locationMaster)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.LocationMasters.Add(locationMaster);
+                await _context.SaveChangesAsync();
+                return Ok(new { Message = "LocationMaster data saved successfully!" });
+            }
+
+            return BadRequest(ModelState); // If model validation fails
+        }
+    }
+}
