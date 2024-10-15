f9d361d90316c5c2c1236039268c3e14f9bb2857
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-15 16:12:12 +0530

    Program1.cs

--- Program1.cs
+++ Program1.cs
@@ -1,16 +1,63 @@
 using System;
-using System.Linq;
 
 class Program
 {
+    static int N = 8;
+
     static void Main()
     {
-        int[] numbers = { 1, 2, 3, 4, 5, 1, 2, 6, 7, 8, 9, 3 };
+        int[,] board = new int[N, N];
+        SolveNQueens(board, 0);
+    }
 
-        var duplicates = numbers.GroupBy(n => n)
-                                .Where(g => g.Count() > 1)
-                                .Select(g => g.Key);
+    static bool IsSafe(int[,] board, int row, int col)
+    {
+        for (int i = 0; i < col; i++)
+            if (board[row, i] == 1)
+                return false;
 
-        Console.WriteLine("Duplicate numbers: " + string.Join(", ", duplicates));
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i, j] == 1)
+                return false;
+
+        for (int i = row, j = col; i < N && j >= 0; i++, j--)
+            if (board[i, j] == 1)
+                return false;
+
+        return true;
+    }
+
+    static bool SolveNQueens(int[,] board, int col)
+    {
+        if (col >= N)
+        {
+            PrintBoard(board);
+            return true;
+        }
+
+        for (int i = 0; i < N; i++)
+        {
+            if (IsSafe(board, i, col))
+            {
+                board[i, col] = 1;
+
+                if (SolveNQueens(board, col + 1))
+                    return true;
+
+                board[i, col] = 0;
+            }
+        }
+
+        return false;
+    }
+
+    static void PrintBoard(int[,] board)
+    {
+        for (int i = 0; i < N; i++)
+        {
+            for (int j = 0; j < N; j++)
+                Console.Write(board[i, j] + " ");
+            Console.WriteLine();
+        }
     }
 }
