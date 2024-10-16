e5cefda802d40d5fc07295843d51eb7cf6fcb544
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 10:48:06 +0530

    added class1.vb

---
+++ Class1.vb
@@ -0,0 +1,23 @@
+﻿Imports System
+Imports System.Text
+
+Module Module1
+    Sub Main()
+        Dim length As Integer = 12
+        Dim password As String = GeneratePassword(length)
+        Console.WriteLine("Generated Password: " & password)
+    End Sub
+
+    Function GeneratePassword(length As Integer) As String
+        Const validChars As String = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
+        Dim res As New StringBuilder()
+        Dim rnd As New Random()
+
+        While length > 0
+            res.Append(validChars(rnd.Next(validChars.Length)))
+            length -= 1
+        End While
+
+        Return res.ToString()
+    End Function
+End Module
