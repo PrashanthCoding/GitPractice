6cd0d70ccf993e123a47853e1dc6012c5ef8ed0b
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 12:01:30 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,35 +1,33 @@
 /*
- * C# Program to Demonstrate the Difference between 
- * Value Type Parameter and Reference Type Parameter
+ *  C# Program to Illustrate Single Inheritance
  */
 using System;
-namespace Example1
+namespace Inheritance
 {
     class Program
     {
-        public static void value(int num)
-        {
-            num++;
-        }
-        public static void reference(ref int num)
-        {
-            num++;
-        }
-
         static void Main(string[] args)
         {
-            int num;
-            Console.Write("Enter a Number:\t");
-            num = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("\n\tValue Type:");
-            Console.Write("\nPrevious Value:\t{0}", num);
-            Program.value(num);
-            Console.Write("\nCurrent Value:\t{0}", num);
-            Console.WriteLine("\n\tReference Type");
-            Console.Write("\nPrevious Value:\t{0}", num);
-            Program.reference(ref num);
-            Console.Write("\nCurrent Value:\t{0}", num);
-            Console.ReadLine();
+            Teacher d = new Teacher();
+            d.Teach();
+            Student s = new Student();
+            s.Learn();
+            s.Teach();
+            Console.ReadKey();
+        }
+        class Teacher
+        {
+            public void Teach()
+            {
+                Console.WriteLine("Teach");
+            }
+        }
+        class Student : Teacher
+        {
+            public void Learn()
+            {
+                Console.WriteLine("Learn");
+            }
         }
     }
 }
