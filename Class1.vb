31788ad9ebabb443a084f719db4b17836ed0a737
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-16 10:55:25 +0530

    Update Class1.vb

--- Class1.vb
+++ Class1.vb
@@ -1,1 +1,19 @@
-﻿#Const
+Module Module1
+    Sub Main()
+        Dim arr() As Integer = {10, 20, 4, 45, 99}
+
+        Dim largest As Integer = Integer.MinValue
+        Dim secondLargest As Integer = Integer.MinValue
+
+        For Each num As Integer In arr
+            If num > largest Then
+                secondLargest = largest
+                largest = num
+            ElseIf num > secondLargest AndAlso num <> largest Then
+                secondLargest = num
+            End If
+        Next
+
+        Console.WriteLine("The second largest element is " & secondLargest)
+    End Sub
+End Module
