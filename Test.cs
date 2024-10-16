728349c1c1412c031beb5e725be680e4b509bf0c
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 12:27:19 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,41 +1,27 @@
 ﻿/*
- *  C# Program to Implement Let Condition using LINQ
+ * C# Program to Illustrate Regular Expression Pattern
  */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Student
+using System.Text.RegularExpressions;
+namespace Application
 {
-    public string Name { get; set; }
-    public string Regno { get; set; }
-    public int Marks { get; set; }
-
-}
-class Program
-{
-    static void Main(string[] args)
+    class Program
     {
-        //Object Initialization for Student class
-        List<Student> objStudent = new List<Student>{
-                    new Student{ Name="Prashanth",  Regno="R001",   Marks=80},
-                    new Student{ Name="Manoj",     Regno="R002",   Marks=40},
-                    new Student{ Name="Nazriya",   Regno="R003",   Marks=25},
-                    new Student{ Name="Darini",    Regno="R004",   Marks=30},
-                    new Student{ Name="Arjun",     Regno="R005",   Marks=70},
-                    new Student{ Name = "Vijay",   Regno = "R006", Marks = 67}
-                };
-
-        var objresult = from stu in objStudent
-                        let totalMarks = objStudent.Sum(mark => mark.Marks)
-                        let avgMarks = totalMarks / 5
-                        where avgMarks > stu.Marks
-                        select stu;
-        foreach (var stu in objresult)
+        private static void showMatch(string text, string expr)
         {
-            Console.WriteLine("Student: {0} {1}", stu.Name, stu.Regno);
-
+            Console.WriteLine("The Expression : " + expr);
+            MatchCollection m = Regex.Matches(text, expr);
+            foreach (Match m1 in m)
+            {
+                Console.WriteLine(m1);
+            }
         }
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            string str = "Sanfoundry , a high end Technology Training company";
+            Console.WriteLine("Matching words that start with 'S': ");
+            showMatch(str, @"\bS\S*");
+            Console.ReadKey();
+        }
     }
 }
