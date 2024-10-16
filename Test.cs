2b42a03ee530bc44e839d56e6659708521466c95
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 14:25:13 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,20 +1,21 @@
 ﻿/*
- * C# Program to Demonstrate Pass by Reference Parameter 
+ * C# Program to Find Magnitude of Integer
  */
 using System;
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main()
     {
-        int val;
-        val = 4;
-        Console.WriteLine("Value Before : {0}", val);
-        square(ref val);
-        Console.WriteLine("Value After : {0}", val);
+        int num, mag = 0;
+        Console.WriteLine("Enter the Number : ");
+        num = int.Parse(Console.ReadLine());
+        Console.WriteLine("Number: " + num);
+        while (num > 0)
+        {
+            mag++;
+            num = num / 10;
+        };
+        Console.WriteLine("Magnitude: " + mag);
         Console.Read();
-    }
-    static void square(ref int refParam)
-    {
-        refParam *= refParam;
     }
 }
