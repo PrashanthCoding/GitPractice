45b9a44df5d0e1fd850a88eb00c16e95fa70e623
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 16:32:01 +0530

    Add more Class files

---
+++ Class17.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * C# Program to Illustrate AutoIncrement Operator using ++
+ */
+using System;
+class Program
+{
+    static void Main()
+    {
+        int i = 0;
+        Console.WriteLine(i);
+        i++;
+        Console.WriteLine(i);
+        i += 3;
+        Console.WriteLine(i);
+        ++i;
+        Console.WriteLine(i);
+        i += i;
+        Console.WriteLine(i);
+        Console.ReadLine();
+    }
+}
