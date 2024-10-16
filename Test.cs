85f43469a8531c7e6d02bd377b00527a99e248ac
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 12:42:59 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,48 +1,18 @@
 ﻿/*
- * C# Program to Generate the Marksheet of the Student
+ * C# Program to Divide Sequence into Groups using LINQ
  */
 using System;
-
-namespace Marksheet1
+using System.Linq;
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            int r, m1, m2, m3, t;
-            float p;
-            string n;
-            Console.WriteLine("Enter Roll Number :");
-            r = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Student Name :");
-            n = Console.ReadLine();
-            Console.WriteLine("Mark of Subject1 : ");
-            m1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Mark of Subject2 : ");
-            m2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Mark of Subject3 : ");
-            m3 = Convert.ToInt32(Console.ReadLine());
-            t = m1 + m2 + m3;
-            p = t / 3.0f;
-            Console.WriteLine("Total : " + t);
-            Console.WriteLine("Percentage : " + p);
-            if (p >= 35 && p < 50)
-            {
-                Console.WriteLine("Grade is C");
-            }
-            if (p >= 50 && p <= 60)
-            {
-                Console.WriteLine("Grade is B");
-            }
-            if (p > 60 && p <= 80)
-            {
-                Console.WriteLine("Grade is A");
-            }
-            if (p > 80 && p <= 100)
-            {
-                Console.WriteLine("Grade is A+");
-            }
-            Console.ReadLine();
-        }
+        var seq = Enumerable.Range(100, 100).Select(x => x / 10f);
+        var grps = from x in seq.Select((i, j) => new { i, Grp = j / 10 })
+                   group x.i by x.Grp into y
+                   select new { Min = y.Min(), Max = y.Max() };
+        foreach (var grp in grps)
+            Console.WriteLine("Min: " + grp.Min + " Max:" + grp.Max);
+        Console.ReadLine();
     }
 }
