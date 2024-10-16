adfe748447f9b72f9481fd833c1b7955abaf4f55
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 11:11:05 +0530

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,9 @@
 using System;
-using System.Text;
 
 class Program
 {
     static void Main()
     {
-        int length = 12;
-        string password = GeneratePassword(length);
-        Console.WriteLine($"Generated Password: {password}");
-    }
-
-    static string GeneratePassword(int length)
-    {
-        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
-        StringBuilder res = new StringBuilder();
-        Random rnd = new Random();
-
-        while (0 < length--)
-        {
-            res.Append(validChars[rnd.Next(validChars.Length)]);
-        }
-        return res.ToString();
+        Console.WriteLine("Hello Prashanth");
     }
 }
