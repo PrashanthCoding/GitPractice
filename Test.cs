cf589bbddf129f7f78733aa5506b1a87b6221ec8
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 16:21:54 +0530

    Script file deleted

--- Test.cs
+++ Test.cs
@@ -1,62 +1,20 @@
-﻿using System;
-class Exercise
+﻿/*
+ * C# Program to Demonstrate DivideByZero Exception
+ */
+using System;
+class Program
 {
     static void Main()
     {
-        double Num1, Num2;
-        double Result = 0.00;
-        char op;
         try
         {
-            Console.Write("Enter your First Number :  ");
-            Num1 = double.Parse(Console.ReadLine());
-            Console.Write("Enter an Operator  (+, -, * or /): ");
-            op = char.Parse(Console.ReadLine());
-            if (op != '+' && op != '-' &&
-                op != '*' && op != '/')
-                throw new Exception(op.ToString());
-            Console.Write("Enter your Second Number :");
-            Num2 = double.Parse(Console.ReadLine());
-            if (op == '/')
-                if (Num2 == 0)
-                    throw new DivideByZeroException("Division by zero is not allowed");
-            Result = Calculator(Num1, Num2, op);
-            Console.WriteLine("\n{0} {1} {2} = {3}", Num1, op, Num2, Result);
+            int result = 15 / int.Parse("0");
+            Console.WriteLine(result);
         }
-        catch (FormatException)
+        catch (DivideByZeroException e)
         {
-            Console.WriteLine("The number you typed is not valid");
+            Console.Write(e.Message);
+            Console.ReadLine();
         }
-        catch (DivideByZeroException ex)
-        {
-            Console.WriteLine(ex.Message);
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Operation Error: {0} is not a valid op", ex.Message);
-        }
-        Console.Read();
     }
-}
-
-static double Calculator(double v1, double v2, char op)
-{
-    double Result = 0.00;
-
-    switch (op)
-    {
-        case '+':
-            Result = v1 + v2;
-            break;
-        case '-':
-            Result = v1 - v2;
-            break;
-        case '*':
-            Result = v1 * v2;
-            break;
-        case '/':
-            Result = v1 / v2;
-            break;
-    }
-    return Result;
 }
