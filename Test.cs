b0f97696ad1614842985f52d7c355c2d4c2f6990
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 12:55:21 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,20 +1,32 @@
-﻿/*
- * C# Program to Create an Instance of StackTrace and to Get all Frames
- */
-using System;
-using System.Diagnostics;
-class program
+﻿using System;
+delegate void dele(string s);
+class TestClass
 {
-    public static void Main()
+    static void Good(string s)
     {
-        StackTrace stackTrace = new StackTrace();
-        StackFrame[] stackFrames = stackTrace.GetFrames();
-        // write call stack method names
-        Console.WriteLine("Method Names : ");
-        foreach (StackFrame stackFrame in stackFrames)
-        {
-            Console.WriteLine(stackFrame.GetMethod().Name);
-        }
-        Console.Read();
+        System.Console.WriteLine("  Good, {0}!", s);
+    }
+
+    static void Morning(string s)
+    {
+        System.Console.WriteLine("  Morning, {0}!", s);
+    }
+
+    static void Main()
+    {
+        dele firstdel, secondDel, multiDel, multiMinusfirstdel;
+        firstdel = Good;
+        secondDel = Morning;
+        multiDel = firstdel + secondDel;
+        multiMinusfirstdel = multiDel - firstdel;
+        Console.WriteLine("Invoking delegate firstdel:");
+        firstdel("A");
+        Console.WriteLine("Invoking delegate secondDel:");
+        secondDel("B");
+        Console.WriteLine("Invoking delegate multiDel:");
+        multiDel("C");
+        Console.WriteLine("Invoking delegate multiMinusfirstdel:");
+        multiMinusfirstdel("D");
+        Console.ReadLine();
     }
 }
