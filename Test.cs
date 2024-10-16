1c2cdb35651312f7182d6b6f7b25db32c69cfaf9
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-16 12:36:31 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,27 +1,47 @@
 ﻿/*
- * C# Program to Count File Extensions and Group it using LINQ
+ * C# Program to Generate the Marksheet of the Student
  */
 using System;
-using System.IO;
-using System.Linq;
-namespace ConsoleApplication9
+
+namespace Marksheet1
 {
     class Program
     {
-        public static void Main()
+        static void Main(string[] args)
         {
-            string[] arr = { "aaa.txt", "bbb.TXT", "xyz.abc.pdf", "aaaa.PDF",
-                             "abc.xml", "ccc.txt", "zzz.txt" };
-            var egrp = arr.Select(file => Path.GetExtension(file).TrimStart('.').ToLower())
-                       .GroupBy(x => x, (ext, extCnt) => new
-                       {
-                           Extension = ext,
-                           Count = extCnt.Count()
-                       });
-
-            foreach (var v in egrp)
-                Console.WriteLine("{0} File(s) with {1} Extension ",
-                                    v.Count, v.Extension);
+            int r, m1, m2, m3, t;
+            float p;
+            string n;
+            Console.WriteLine("Enter Roll Number :");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Student Name :");
+            n = Console.ReadLine();
+            Console.WriteLine("Mark of Subject1 : ");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of Subject2 : ");
+            m2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mark of Subject3 : ");
+            m3 = Convert.ToInt32(Console.ReadLine());
+            t = m1 + m2 + m3;
+            p = t / 3.0f;
+            Console.WriteLine("Total : " + t);
+            Console.WriteLine("Percentage : " + p);
+            if (p >= 35 && p < 50)
+            {
+                Console.WriteLine("Grade is C");
+            }
+            if (p >= 50 && p <= 60)
+            {
+                Console.WriteLine("Grade is B");
+            }
+            if (p > 60 && p <= 80)
+            {
+                Console.WriteLine("Grade is A");
+            }
+            if (p > 80 && p <= 100)
+            {
+                Console.WriteLine("Grade is A+");
+            }
             Console.ReadLine();
         }
     }
