0f4bf05430d2f92c9c17c1185e350d9ff9085fe7
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-17 12:29:40 +0530

    Script.js file modified

--- Script1.js
+++ Script1.js
@@ -1,17 +1,4 @@
-<html>
-<head>
-    <title> Using the strict mode gloablly </title>
-</head>
-<body>
-    <script>
-        x = 100; // This is valid
-        document.write("The value of the X is - " + x);
-        function test() {
-            "use strict";
-            y = 50; // This is not valid
-            document.write("The value of the y is: " + x);
-        }
-        test();
-    </script>
-</body>
-</html>
+<script>
+    'use strict';
+    num = 70.90; // This is invalid
+</script>
