63bf00e409107e7beb97e007eea25308e4d33721
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-17 11:19:45 +0530

    test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,77 +1,22 @@
 ﻿/*
- * C# Program to Perform Matrix Addition 
+ * C# Program to Reverse a String without using Reverse function
  */
 using System;
-
-namespace ConsoleApplication8
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        string Str, reversestring = "";
+        int Length;
+        Console.Write("Enter A String : ");
+        Str = Console.ReadLine();
+        Length = Str.Length - 1;
+        while (Length >= 0)
         {
-            int m, n, i, j;
-            Console.Write("Enter Number Of Rows And Columns Of Matrices A and B : ");
-            m = Convert.ToInt16(Console.ReadLine());
-            n = Convert.ToInt16(Console.ReadLine());
-            int[,] A = new int[10, 10];
-            Console.Write("\nEnter The First Matrix : ");
-            for (i = 0; i < m; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    A[i, j] = Convert.ToInt16(Console.ReadLine());
-                }
-            }
-
-            int[,] B = new int[10, 10];
-            Console.Write("\nEnter The Second Matrix:");
-            for (i = 0; i < m; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    B[i, j] = Convert.ToInt16(Console.ReadLine());
-                }
-            }
-            Console.Clear();
-            Console.WriteLine("\nMatrix A : ");
-            for (i = 0; i < m; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    Console.Write(A[i, j] + "\t");
-
-                }
-                Console.WriteLine();
-            }
-            Console.WriteLine("\nMatrix B: ");
-            for (i = 0; i < m; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    Console.Write(B[i, j] + "\t");
-
-                }
-                Console.WriteLine();
-            }
-            int[,] C = new int[10, 10];
-            for (i = 0; i < m; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    C[i, j] = A[i, j] + B[i, j];
-                }
-            }
-            Console.Write("\nSum Matrix :");
-            for (i = 0; i < m; i++)
-            {
-                for (j = 0; j < n; j++)
-                {
-                    Console.Write(C[i, j] + "\t");
-
-                }
-                Console.WriteLine();
-            }
-            Console.Read();
+            reversestring = reversestring + Str[Length];
+            Length--;
         }
+        Console.WriteLine("Reverse  String  Is  {0}", reversestring);
+        Console.ReadLine();
     }
 }
