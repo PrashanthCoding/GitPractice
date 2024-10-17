ca832b36df3d2ae47556f1909615c0fd299cc7cf
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-17 17:06:47 +0530

    Class1.cs modified

--- Class1.cs
+++ Class1.cs
@@ -1,25 +1,83 @@
 ﻿/*
- * C# Program to Sort a List of Names in Alphabetical Order
+ * C# Program to Perform Radix Sort
  */
 using System;
 using System.Collections.Generic;
-namespace ConsoleApplication1
+
+namespace ConsoleApplication2
 {
-    class Program
+    class Example
     {
-        static void Main()
+        private int[] data;
+        private IList<IList<int>> digits = new List<IList<int>>();
+        private int maxLength = 0;
+        public Example()
         {
-            List<string> names = new List<string>();
-            names.Add("Ram");
-            names.Add("Rose");
-            names.Add("Abs");
-            names.Add("Edward");
-            names.Add("Sita");
-            names.Add("Prashanth");
-            names.Add("Darini");
-            names.Sort();
-            foreach (string s in names)
-                Console.WriteLine(s);
+            for (int i = 0; i < 10; i++)
+            {
+                digits.Add(new List<int>());
+            }
+            Console.Write("Enter the Number of Records : ");
+            int count = int.Parse(Console.ReadLine());
+            data = new int[count];
+            Console.ReadLine();
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("Enter Record {0} : ", i + 1);
+
+                data[i] = int.Parse(Console.ReadLine());
+
+                if (maxLength < data[i].ToString().Length)
+                    maxLength = data[i].ToString().Length;
+            }
+        }
+
+        public void RadixSort()
+        {
+            for (int i = 0; i < maxLength; i++)
+            {
+                for (int j = 0; j < data.Length; j++)
+                {
+                    int digit = (int)((data[j] % Math.Pow(10, i + 1)) / Math.Pow(10, i));
+
+                    digits[digit].Add(data[j]);
+                }
+
+                int index = 0;
+                for (int k = 0; k < digits.Count; k++)
+                {
+                    IList<int> selDigit = digits[k];
+
+                    for (int l = 0; l < selDigit.Count; l++)
+                    {
+                        data[index++] = selDigit[l];
+                    }
+                }
+                ClearDigits();
+            }
+            printSortedData();
+        }
+
+        private void ClearDigits()
+        {
+            for (int k = 0; k < digits.Count; k++)
+            {
+                digits[k].Clear();
+            }
+        }
+
+        public void printSortedData()
+        {
+            Console.WriteLine("The Sorted Numbers are : ");
+            for (int i = 0; i < data.Length; i++)
+            {
+                Console.WriteLine(data[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            new Example().RadixSort();
+
             Console.ReadLine();
         }
     }
