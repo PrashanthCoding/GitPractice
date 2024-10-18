9d3235ac897d6fd94445401016e6ce7dd3d61ff8
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-18 09:51:11 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,22 +1,45 @@
 using System;
-class Demo
+namespace DotNetTutorials
 {
-    public static void Main()
+    public class ReversalAlgorithm
     {
-        int decValue, counter, i = 1, j;
-        int[] octalValue = new int[80];
-        decValue = 127;
-        counter = decValue;
-        Console.WriteLine("Decimal Number:{0}", decValue);
-        while (counter != 0)
+        static void Main(string[] args)
         {
-            octalValue[i++] = counter % 8;
-            counter /= 8;
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            Console.WriteLine("Original Array is :");
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.WriteLine();
+
+            Console.Write("Enter value of k:");
+            int position = Convert.ToInt32(Console.ReadLine());
+            ReversalAlgorithm obj = new ReversalAlgorithm();
+            obj.SwapElements(arr, 0, (position - 1));// 1st part
+            obj.SwapElements(arr, position, (arr.Length - 1));//2nd 
+            obj.SwapElements(arr, 0, (arr.Length - 1));
+
+            Console.WriteLine();
+
+            Console.WriteLine("Rotation of array by position " + position);
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Console.ReadKey();
         }
 
-        Console.Write("Octal Number: ");
-        for (j = i - 1; j > 0; j--)
-            Console.Write(octalValue[j]);
-        Console.Read();
+        //3 times
+        void SwapElements(int[] arr, int startIndex, int endIndex)
+        {
+            while (startIndex < endIndex)
+            {
+                int temp = arr[startIndex];
+                arr[startIndex] = arr[endIndex];
+                arr[endIndex] = temp;
+
+                startIndex++;
+                endIndex--;
+            }
+        }
     }
 }
