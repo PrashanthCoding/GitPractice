79ebbc6eff89181d4c8dc4c12db0c7902f60e673
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-18 09:58:09 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,40 +1,38 @@
 using System;
-namespace DotNetTutorials
+
+namespace ReverseAnArrayDemo
 {
-    public class ReversalAlgorithm
+    public class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-            Console.WriteLine("Original Array is :");
+            //Creating an Integer Array
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            Console.Write("Original Array Elements :");
             for (int i = 0; i < arr.Length; i++)
+            {
                 Console.Write(arr[i] + " ");
+            }
 
-            Console.WriteLine();
+            ReverseElements(arr, 0, (arr.Length - 1));
 
-            Console.Write("Enter value of k:");
-            int position = Convert.ToInt32(Console.ReadLine());
-            ReversalAlgorithm obj = new ReversalAlgorithm();
-            obj.SwapElements(arr, 0, (position - 1));// 1st part
-            obj.SwapElements(arr, position, (arr.Length - 1));//2nd 
-            obj.SwapElements(arr, 0, (arr.Length - 1));
-
-            Console.WriteLine();
-
-            Console.WriteLine("Rotation of array by position " + position);
+            Console.Write("\nReversed Array Elements :");
             for (int i = 0; i < arr.Length; i++)
+            {
                 Console.Write(arr[i] + " ");
+            }
 
             Console.ReadKey();
         }
 
-        //3 times
-        void SwapElements(int[] arr, int startIndex, int endIndex)
+        public static void ReverseElements(int[] arr, int startIndex, int endIndex)
         {
             while (startIndex < endIndex)
             {
                 int temp = arr[startIndex];
                 arr[startIndex] = arr[endIndex];
+
                 arr[endIndex] = temp;
 
                 startIndex++;
