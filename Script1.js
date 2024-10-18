3a40f371b04727c5482f1a1edd2dccf7039def59
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-18 11:48:58 +0530

    Script.js modified and Class1.cs deleted

--- Script1.js
+++ Script1.js
@@ -1,29 +1,63 @@
-function showTime() {
-    var date = new Date();
-    var h = date.getHours(); // 0 - 23
-    var m = date.getMinutes(); // 0 - 59
-    var s = date.getSeconds(); // 0 - 59
-    var session = "AM";
+// IEFE
+(() => {
+    // state variables
+    let toDoListArray = [];
+    // ui variables
+    const form = document.querySelector(".form");
+    const input = form.querySelector(".form__input");
+    const ul = document.querySelector(".toDoList");
 
-    if (h == 0) {
-        h = 12;
+    // event listeners
+    form.addEventListener('submit', e => {
+        // prevent default behaviour - Page reload
+        e.preventDefault();
+        // give item a unique ID
+        let itemId = String(Date.now());
+        // get/assign input value
+        let toDoItem = input.value;
+        //pass ID and item into functions
+        addItemToDOM(itemId, toDoItem);
+        addItemToArray(itemId, toDoItem);
+        // clear the input box. (this is default behaviour but we got rid of that)
+        input.value = '';
+    });
+
+    ul.addEventListener('click', e => {
+        let id = e.target.getAttribute('data-id')
+        if (!id) return // user clicked in something else      
+        //pass id through to functions
+        removeItemFromDOM(id);
+        removeItemFromArray(id);
+    });
+
+    // functions 
+    function addItemToDOM(itemId, toDoItem) {
+        // create an li
+        const li = document.createElement('li')
+        li.setAttribute("data-id", itemId);
+        // add toDoItem text to li
+        li.innerText = toDoItem
+        // add li to the DOM
+        ul.appendChild(li);
     }
 
-    if (h > 12) {
-        h = h - 12;
-        session = "PM";
+    function addItemToArray(itemId, toDoItem) {
+        // add item to array as an object with an ID so we can find and delete it later
+        toDoListArray.push({ itemId, toDoItem });
+        console.log(toDoListArray)
     }
 
-    h = (h < 10) ? "0" + h : h;
-    m = (m < 10) ? "0" + m : m;
-    s = (s < 10) ? "0" + s : s;
+    function removeItemFromDOM(id) {
+        // get the list item by data ID
+        var li = document.querySelector('[data-id="' + id + '"]');
+        // remove list item
+        ul.removeChild(li);
+    }
 
-    var time = h + ":" + m + ":" + s + " " + session;
-    document.getElementById("MyClockDisplay").innerText = time;
-    document.getElementById("MyClockDisplay").textContent = time;
+    function removeItemFromArray(id) {
+        // create a new toDoListArray with all li's that don't match the ID
+        toDoListArray = toDoListArray.filter(item => item.itemId !== id);
+        console.log(toDoListArray);
+    }
 
-    setTimeout(showTime, 1000);
-
-}
-
-showTime();
+})();
