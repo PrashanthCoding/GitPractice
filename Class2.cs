40f77d99f74feb6f5779f2ba1cc27de5002e248b
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-19 10:28:23 +0530

    Update Class2.cs

--- Class2.cs
+++ Class2.cs
@@ -1,1 +1,19 @@
-Hi da Prashanth
+using System;
+class Fibonacci
+{
+    static void Main()
+    {
+        int n1 = 0, n2 = 1, n3, number;
+        Console.Write("Enter the number of elements: ");
+        number = int.Parse(Console.ReadLine());
+        Console.Write(n1 + " " + n2); // Print 0 and 1
+        
+        for (int i = 2; i < number; ++i)
+        {
+            n3 = n1 + n2;
+            Console.Write(" " + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+}
