45c103e86715835c9b4b4b8879e35d944ab15280
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-19 10:45:42 +0530

    Test.cs added

---
+++ Test.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Factorial
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        int fact = 1;
+
+        for (int i = 1; i <= number; i++)
+        {
+            fact *= i;
+        }
+
+        Console.WriteLine("Factorial of " + number + " is: " + fact);
+    }
+}
