733ad91c447bfc4eec39f081bb3ab0636de21caf
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-21 10:27:07 +0530

    Class1.cs delete

--- Test.cs
+++ Test.cs
@@ -1,16 +1,53 @@
-﻿
-// Start date
-using System;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-DateTime startDate = new DateTime(2005, 2, 1, 3, 4, 12, 56);
-// End date
-DateTime endDate = new DateTime(2005, 12, 12, 4, 30, 45, 12);
-// Time span
-TimeSpan diffDate = endDate.Subtract(startDate);
-// Spit it out
-Console.WriteLine("Time Difference: ");
-Console.WriteLine(diffDate.Days.ToString() + " Days");
-Console.WriteLine(diffDate.Hours.ToString() + " Hours");
-Console.WriteLine(diffDate.Minutes.ToString() + " Minutes");
-Console.WriteLine(diffDate.Seconds.ToString() + " Seconds ");
-Console.WriteLine(diffDate.Milliseconds.ToString() + " Milliseconds ");
+namespace exercises
+{
+    class Program
+    {
+        // Main method - entry point of the program
+        static void Main(string[] args)
+        {
+            // Initialize a string variable with some text content
+            string text = "CPP Exercises.";
+
+            // Display the original string
+            Console.WriteLine("Original string: " + text);
+
+            // Calculate and display the average word length in the string
+            Console.WriteLine("The average word length in the said string: " + test(text));
+
+            // Update the string variable with a different text content
+            text = "C# syntax is highly expressive, yet it is also simple and easy to learn.";
+
+            // Display the updated string
+            Console.WriteLine("Original string: " + text);
+
+            // Calculate and display the average word length in the updated string
+            Console.WriteLine("The average word length in the said string: " + test(text));
+
+            // Update the string variable with another text content
+            text = "C# is an elegant and type-safe object-oriented language";
+
+            // Display the updated string
+            Console.WriteLine("Original string: " + text);
+
+            // Calculate and display the average word length in the updated string
+            Console.WriteLine("The average word length in the said string: " + test(text));
+        }
+
+        // Method to calculate the average word length in a string
+        public static double test(string text)
+        {
+            // Remove non-alphabetic characters and keep spaces to isolate words
+            string new_text = Regex.Replace(text, "[^A-Za-z ]", "");
+
+            // Split the string into words, calculate the length of each word, find the average length of all words
+            double average_len = new_text.Split(' ').Select(x => x.Length).Average();
+
+            // Round the average word length to 2 decimal places and return the result
+            return Math.Round(average_len, 2);
+        }
+    }
+}
