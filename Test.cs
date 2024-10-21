4c175c6ace0aed75f0eceb5217dd3e87ce674508
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-21 10:13:14 +0530

    Test.cs added

--- Test.cs
+++ Test.cs
@@ -1,17 +1,16 @@
-﻿using System;
+﻿
+// Start date
+using System;
 
-namespace Console_DateTime
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            DateTime TodayNoon = new DateTime(2018, 9, 13, 12, 0, 0);
-            DateTime TodayMidnight = new DateTime(2018, 9, 12, 0, 0, 0);
-            TimeSpan Result = TodayNoon.Subtract(TodayMidnight);
-            Console.WriteLine($"Yesterday Midnight - Today Noon = {Result.Days}");
-            Console.WriteLine($"Yesterday Midnight - Today Noon = {Result.TotalDays}");
-            Console.ReadLine();
-        }
-    }
-}
+DateTime startDate = new DateTime(2005, 2, 1, 3, 4, 12, 56);
+// End date
+DateTime endDate = new DateTime(2005, 12, 12, 4, 30, 45, 12);
+// Time span
+TimeSpan diffDate = endDate.Subtract(startDate);
+// Spit it out
+Console.WriteLine("Time Difference: ");
+Console.WriteLine(diffDate.Days.ToString() + " Days");
+Console.WriteLine(diffDate.Hours.ToString() + " Hours");
+Console.WriteLine(diffDate.Minutes.ToString() + " Minutes");
+Console.WriteLine(diffDate.Seconds.ToString() + " Seconds ");
+Console.WriteLine(diffDate.Milliseconds.ToString() + " Milliseconds ");
