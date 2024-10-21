545f225ab6493b73a9bcbf431eb5048b3ba256a7
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-21 10:05:51 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,20 +1,17 @@
 ﻿using System;
-class HelloWorld
+
+namespace Console_DateTime
 {
-    static void Main()
+    class Program
     {
-        Console.WriteLine("Enter a Number 1: ");
-        int a = Console.ReadLine();
-        Console.WriteLine("Enter a Number 2: ");
-        int b = Console.ReadLine();
-        int result = a + b;
-        int result1 = a - b;
-        int result2 = a * b;
-        int result3 = a / b;
-        Console.WriteLine($"Result is : {result}");
-        Console.WriteLine($"Result1 is : {result1}");
-        Console.WriteLine($"Result2 is : {result2}");
-        Console.WriteLine($"Result3 is : {result3}");
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            DateTime dtTodayNoon = new DateTime(2018, 9, 13, 12, 0, 0);
+            DateTime dtTodayMidnight = new DateTime(2018, 9, 12, 0, 0, 0);
+            TimeSpan diffResult = dtTodayNoon.Subtract(dtTodayMidnight);
+            Console.WriteLine($"Yesterday Midnight - Today Noon = {diffResult.Days}");
+            Console.WriteLine($"Yesterday Midnight - Today Noon = {diffResult.TotalDays}");
+            Console.ReadLine();
+        }
     }
 }
