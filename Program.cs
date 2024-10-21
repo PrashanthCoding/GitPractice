3ef2f9078d7a2f889dd4c1613814dec77d7fd787
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-21 15:16:50 +0530

    test

--- Program.cs
+++ Program.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 
-class FruitSelection
+class GradingSystem
 {
     static void Main()
     {
-        Console.Write("Choose a fruit (apple, banana, mango): ");
-        string fruit = Console.ReadLine().ToLower();
+        Console.Write("Enter marks (0-100): ");
+        int marks = int.Parse(Console.ReadLine());
 
-        switch (fruit)
+        switch (marks / 10)
         {
-            case "apple": Console.WriteLine("Apple is red or green."); break;
-            case "banana": Console.WriteLine("Banana is yellow."); break;
-            case "mango": Console.WriteLine("Mango is sweet."); break;
-            default: Console.WriteLine("Unknown fruit!"); break;
+            case 9:
+            case 10: Console.WriteLine("Grade: A"); break;
+            case 8: Console.WriteLine("Grade: B"); break;
+            case 7: Console.WriteLine("Grade: C"); break;
+            case 6: Console.WriteLine("Grade: D"); break;
+            default: Console.WriteLine("Grade: F"); break;
         }
     }
 }
