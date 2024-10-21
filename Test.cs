c24f3be6bf998e735f9bfeecf6a05b036d465767
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-21 11:21:49 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,66 +1,38 @@
 using System;
 
-namespace SwitchCaseExample
+class DaysOfWeek
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.Write("Enter a number (1-7) for a day: ");
+        int day = int.Parse(Console.ReadLine());
+
+        switch (day)
         {
-            Console.WriteLine("Simple Calculator using Switch Case");
-            Console.WriteLine("Enter the first number: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Enter the second number: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine("Choose an operation: ");
-            Console.WriteLine("1: Addition (+)");
-            Console.WriteLine("2: Subtraction (-)");
-            Console.WriteLine("3: Multiplication (*)");
-            Console.WriteLine("4: Division (/)");
-
-            Console.Write("Enter your choice (1-4): ");
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            double result = 0;
-            bool validOperation = true;
-
-            switch (choice)
-            {
-                case 1:
-                    result = num1 + num2;
-                    Console.WriteLine("Result: " + num1 + " + " + num2 + " = " + result);
-                    break;
-                case 2:
-                    result = num1 - num2;
-                    Console.WriteLine("Result: " + num1 + " - " + num2 + " = " + result);
-                    break;
-                case 3:
-                    result = num1 * num2;
-                    Console.WriteLine("Result: " + num1 + " * " + num2 + " = " + result);
-                    break;
-                case 4:
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                        Console.WriteLine("Result: " + num1 + " / " + num2 + " = " + result);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Division by zero is not allowed.");
-                        validOperation = false;
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please select between 1 and 4.");
-                    validOperation = false;
-                    break;
-            }
-
-            if (validOperation)
-            {
-                Console.WriteLine("Calculation successful!");
-            }
+            case 1:
+                Console.WriteLine("Sunday");
+                break;
+            case 2:
+                Console.WriteLine("Monday");
+                break;
+            case 3:
+                Console.WriteLine("Tuesday");
+                break;
+            case 4:
+                Console.WriteLine("Wednesday");
+                break;
+            case 5:
+                Console.WriteLine("Thursday");
+                break;
+            case 6:
+                Console.WriteLine("Friday");
+                break;
+            case 7:
+                Console.WriteLine("Saturday");
+                break;
+            default:
+                Console.WriteLine("Invalid day!");
+                break;
         }
     }
 }
