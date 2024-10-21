aa8ee75f68cce9943c4b7b1a34f94058c08ca803
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-21 10:10:15 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -6,11 +6,11 @@ namespace Console_DateTime
     {
         static void Main(string[] args)
         {
-            DateTime dtTodayNoon = new DateTime(2018, 9, 13, 12, 0, 0);
-            DateTime dtTodayMidnight = new DateTime(2018, 9, 12, 0, 0, 0);
-            TimeSpan diffResult = dtTodayNoon.Subtract(dtTodayMidnight);
-            Console.WriteLine($"Yesterday Midnight - Today Noon = {diffResult.Days}");
-            Console.WriteLine($"Yesterday Midnight - Today Noon = {diffResult.TotalDays}");
+            DateTime TodayNoon = new DateTime(2018, 9, 13, 12, 0, 0);
+            DateTime TodayMidnight = new DateTime(2018, 9, 12, 0, 0, 0);
+            TimeSpan Result = TodayNoon.Subtract(TodayMidnight);
+            Console.WriteLine($"Yesterday Midnight - Today Noon = {Result.Days}");
+            Console.WriteLine($"Yesterday Midnight - Today Noon = {Result.TotalDays}");
             Console.ReadLine();
         }
     }
