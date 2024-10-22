23c89aeec7b76bff8bfe3416cefb47c928f68830
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-22 14:39:15 +0530

    s

--- Program.cs
+++ Program.cs
@@ -1,37 +1,19 @@
 ﻿using System;
-
-class ShapeAreaCalculator
+class Fibonacci
 {
     static void Main()
     {
-        Console.WriteLine("Choose a shape (circle, square, rectangle): ");
-        string shape = Console.ReadLine().ToLower();
+        int n1 = 0, n2 = 1, n3, number;
+        Console.Write("Enter the number of elements: ");
+        number = int.Parse(Console.ReadLine());
+        Console.Write(n1 + " " + n2); // Print 0 and 1
 
-        switch (shape)
+        for (int i = 2; i < number; ++i)
         {
-            case "circle":
-                Console.Write("Enter the radius: ");
-                double radius = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Circle: {Math.PI * radius * radius}");
-                break;
-
-            case "square":
-                Console.Write("Enter the side length: ");
-                double side = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Square: {side * side}");
-                break;
-
-            case "rectangle":
-                Console.Write("Enter the length: ");
-                double length = double.Parse(Console.ReadLine());
-                Console.Write("Enter the width: ");
-                double width = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Rectangle: {length * width}");
-                break;
-
-            default:
-                Console.WriteLine("Unknown shape!");
-                break;
+            n3 = n1 + n2;
+            Console.Write(" " + n3);
+            n1 = n2;
+            n2 = n3;
         }
     }
 }
