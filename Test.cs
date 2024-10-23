54268e816b3a2fa9ec27956fa73f12308cf02968
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-23 15:43:57 +0530

    s

--- Test.cs
+++ Test.cs
@@ -1,37 +1,8 @@
 ﻿using System;
+using System.Linq;
 
-class ShapeAreaCalculator
-{
-    static void Main()
-    {
-        Console.WriteLine("Choose a shape (circle, square, rectangle): ");
-        string shape = Console.ReadLine().ToLower();
+int[] arr = { 1, 2, 3, 4, 5 };
+int n = 2; // Rotate by 2 positions
+int[] rotatedArr = arr.Skip(n).Concat(arr.Take(n)).ToArray();
 
-        switch (shape)
-        {
-            case "circle":
-                Console.Write("Enter the radius: ");
-                double radius = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Circle: {Math.PI * radius * radius}");
-                break;
-
-            case "square":
-                Console.Write("Enter the side length: ");
-                double side = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Square: {side * side}");
-                break;
-
-            case "rectangle":
-                Console.Write("Enter the length: ");
-                double length = double.Parse(Console.ReadLine());
-                Console.Write("Enter the width: ");
-                double width = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Rectangle: {length * width}");
-                break;
-
-            default:
-                Console.WriteLine("Unknown shape!");
-                break;
-        }
-    }
-}
+Console.WriteLine("Array after left rotation: " + string.Join(", ", rotatedArr));
