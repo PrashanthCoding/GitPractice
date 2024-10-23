0720ddc5818c886970ef466d7921003e1e7badbd
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-23 15:14:36 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
 ﻿using System;
 using System.Linq;
 
-int[] arr = { 2, 3, 5, 7, 8 };
-var oddNumbers = arr.Where(x => x % 2 != 0).ToArray();
+bool IsPrime(int num)
+{
+    if (num < 2) return false;
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0) return false;
+    }
+    return true;
+}
 
-Console.WriteLine("Odd numbers: " + string.Join(", ", oddNumbers));
+int[] arr = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var primes = arr.Where(IsPrime).ToArray();
+
+Console.WriteLine("Prime numbers: " + string.Join(", ", primes));
