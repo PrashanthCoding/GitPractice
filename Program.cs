76e3af939cad338c4bea44acd355b9044c2684f6
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-23 10:10:24 +0530

    s

--- Program.cs
+++ Program.cs
@@ -1,37 +1,18 @@
 ﻿using System;
 
-class ShapeAreaCalculator
+class InstrumentSounds
 {
     static void Main()
     {
-        Console.WriteLine("Choose a shape (circle, square, rectangle): ");
-        string shape = Console.ReadLine().ToLower();
+        Console.WriteLine("Enter a musical instrument (guitar, piano, drum): ");
+        string instrument = Console.ReadLine().ToLower();
 
-        switch (shape)
+        switch (instrument)
         {
-            case "circle":
-                Console.Write("Enter the radius: ");
-                double radius = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Circle: {Math.PI * radius * radius}");
-                break;
-
-            case "square":
-                Console.Write("Enter the side length: ");
-                double side = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Square: {side * side}");
-                break;
-
-            case "rectangle":
-                Console.Write("Enter the length: ");
-                double length = double.Parse(Console.ReadLine());
-                Console.Write("Enter the width: ");
-                double width = double.Parse(Console.ReadLine());
-                Console.WriteLine($"Area of Rectangle: {length * width}");
-                break;
-
-            default:
-                Console.WriteLine("Unknown shape!");
-                break;
+            case "guitar": Console.WriteLine("Strum!"); break;
+            case "piano": Console.WriteLine("Pling!"); break;
+            case "drum": Console.WriteLine("Boom!"); break;
+            default: Console.WriteLine("Unknown instrument!"); break;
         }
     }
 }
