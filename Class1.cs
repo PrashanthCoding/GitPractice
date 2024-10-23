3432023f77391980a6875bab0a48ee8a550463ef
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-23 09:42:49 +0530

    S

--- Class1.cs
+++ Class1.cs
@@ -1,26 +1,34 @@
-﻿using System;
+﻿Ranking Method Scores:
+Vendor A: 2.80
+Vendor B: 2.20
+Vendor C: 3.00
 
-class InstrumentSounds
-{
-    static void Main()
-    {
-        Console.WriteLine("Enter a musical instrument (guitar, piano, drum): ");
-        string instrument = Console.ReadLine().ToLower();
+Actual Data Method Scores:
+Vendor A: 1100.00
+Vendor B: 1290.00
+Vendor C: 960.00
 
-        switch (instrument)
-        {
-            case "guitar":
-                Console.WriteLine("Strum!");
-                break;
-            case "piano":
-                Console.WriteLine("Pling!");
-                break;
-            case "drum":
-                Console.WriteLine("Boom!");
-                break;
-            default:
-                Console.WriteLine("Unknown instrument!");
-                break;
-        }
-    }
-}
+Difference Weighted Method Scores:
+Vendor A: 35.00
+Vendor B: 50.00
+Vendor C: -85.00
+
+Normalized Method Scores:
+Vendor A: 0.07
+Vendor B: 0.11
+Vendor C: -0.18
+
+Load Differences(%):
+Vendor A: 8.00
+Vendor B: -12.00
+Vendor C: 5.00
+
+Value Differences(%):
+Vendor A: 8.00
+Vendor B: -12.00
+Vendor C: 5.00
+
+Trip Count Differences (%):
+Vendor A: -5.00
+Vendor B: 0.00
+Vendor C: 5.00
