04ce62588e92198caefba47965723b7c4d20f1e3
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 12:20:19 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -4,27 +4,7 @@ class Program
 {
     static void Main()
     {
-        int number = 10;
-
-        // if-else statement
-        if (number > 0)
-            Console.WriteLine("Positive number");
-        else
-            Console.WriteLine("Negative number");
-
-        // switch statement
-        switch (number)
-        {
-            case 10:
-                Console.WriteLine("Number is 10");
-                break;
-            default:
-                Console.WriteLine("Number is not 10");
-                break;
-        }
-
-        // for loop
-        for (int i = 1; i <= 5; i++)
-            Console.WriteLine(i);
+        Func<int, int, int> add = (x, y) => x + y;
+        Console.WriteLine("Sum: " + add(5, 3));
     }
 }
