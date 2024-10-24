04de495c0af6a250bc87dec99bccf93c6a561148
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 17:49:13 +0530

    Script.js modified

--- Script1.js
+++ Script1.js
@@ -1,9 +1,6 @@
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    fullName: function () {
-        return this.firstName + " " + this.lastName;
-    }
-};
+function elementExists(arr, element) {
+    return arr.includes(element);
+}
 
-console.log(person.fullName());  // Output: John Doe
+console.log(elementExists([1, 2, 3, 4], 3));  // Output: true
+console.log(elementExists([1, 2, 3, 4], 5));  // Output: false
