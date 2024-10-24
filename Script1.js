d93eeeeaacf5ec2ffc42255466584f5d58c9fec9
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 16:04:41 +0530

    Script.js modified

--- Script1.js
+++ Script1.js
@@ -1,5 +1,9 @@
-function reverseString(str) {
-    return str.split("").reverse().join("");
+function sumOfNaturalNumbers(n) {
+    let sum = 0;
+    for (let i = 1; i <= n; i++) {
+        sum += i;
+    }
+    return sum;
 }
 
-console.log(reverseString("hello"));  // Output: olleh
+console.log(sumOfNaturalNumbers(10));  // Output: 55
