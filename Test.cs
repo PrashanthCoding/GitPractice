f0dc5a4a37fa29a36f851f9e49c15b7a381d4009
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 10:02:10 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,8 +1,11 @@
 ﻿using System;
-using System.Linq;
-
-int[] arr = { 1, 2, 3, 4, 5 };
-int n = 2; // Rotate by 2 positions
-int[] rotatedArr = arr.Skip(n).Concat(arr.Take(n)).ToArray();
-
-Console.WriteLine("Array after left rotation: " + string.Join(", ", rotatedArr));
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
