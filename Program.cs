18ff5ab031afffda97741ed782e62ec51eb3f86b
Prashanth <108060806+PrashanthCoding@users.noreply.github.com>
2024-10-24 14:45:34 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿namespace SimpleWebAPI.Controllers
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleWebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DataController : ControllerBase
+    public class PaginatedItemsController : ControllerBase
     {
-        // POST api/data
-        [HttpPost]
-        public IActionResult Post([FromBody] string value)
+        private static List<string> items = Enumerable.Range(1, 100).Select(x => $"Item {x}").ToList();
+
+        // GET api/paginateditems?page=2&pageSize=10
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(int page = 1, int pageSize = 10)
         {
-            return Ok($"Received value: {value}");
+            var paginatedItems = items.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+            return Ok(paginatedItems);
         }
     }
 }
