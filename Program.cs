a5bef38cd486a266242d664658b981c6f154c94d
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 14:34:57 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
 ﻿using System;
+using System.Threading.Tasks;
+
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
-        int a = 0, b = 1, c, i, number;
-        Console.Write("Enter the number of elements: ");
-        number = int.Parse(Console.ReadLine());
+        await PerformTask();
+        Console.WriteLine("Main method completed");
+    }
 
-        Console.Write(a + " " + b + " "); // Print 0 and 1
-
-        for (i = 2; i < number; ++i) // Loop starts from 2 because 0 and 1 are already printed
-        {
-            c = a + b;
-            Console.Write(c + " ");
-            a = b;
-            b = c;
-        }
+    static async Task PerformTask()
+    {
+        Console.WriteLine("Task started...");
+        await Task.Delay(3000);
+        Console.WriteLine("Task completed!");
     }
 }
