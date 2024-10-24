6258ed062ad3b22c92898068b2fe947ea9772c71
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 10:42:42 +0530

    Test.cs modified

--- Test.cs
+++ Test.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class Program
 {
-    static void PrintMessage(string message)
-    {
-        Console.WriteLine("Action Delegate: " + message);
-    }
-
     static void Main()
     {
-        Action<string> action = PrintMessage;
-        action("Hello from Action delegate!");
+        List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+        Predicate<int> isEven = x => x % 2 == 0;
+
+        List<int> evenNumbers = numbers.FindAll(isEven);
+
+        Console.WriteLine("Even numbers: ");
+        evenNumbers.ForEach(Console.WriteLine);
     }
 }
