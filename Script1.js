de3e68121e11deb7f1756371b55d51a8a17be51d
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 17:21:57 +0530

    Script.js modified

--- Script1.js
+++ Script1.js
@@ -1,9 +1,14 @@
-const person = {
-    firstName: "John",
-    lastName: "Doe",
-    fullName: function () {
-        return this.firstName + " " + this.lastName;
+function findLargestIn2DArray(arr) {
+    let largest = Number.NEGATIVE_INFINITY;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+            if (arr[i][j] > largest) {
+                largest = arr[i][j];
+            }
+        }
     }
-};
+    return largest;
+}
 
-console.log(person.fullName());  // Output: John Doe
+const array2D = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+console.log(findLargestIn2DArray(array2D));  // Output: 9
