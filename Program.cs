e46475fd92301b6e60875d438404452cb88ee125
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-24 14:10:34 +0530

    Program.cs modiifed

--- Program.cs
+++ Program.cs
@@ -1,30 +1,27 @@
-﻿namespace MasterDataServices.Models.User
+﻿using System;
+
+class MonthDisplay
 {
-    public class UsertypeUserValidator : AbstractValidator<UsertypeUser>
+    static void Main()
     {
-        public UsertypeUserValidator()
+        Console.Write("Enter a number (1-12) for a month: ");
+        int month = int.Parse(Console.ReadLine());
+
+        switch (month)
         {
-            RuleFor(x => x.user_id)
-                .GreaterThan(0).WithMessage("User ID must be greater than zero.")
-                .NotEmpty().WithMessage("User ID is required.");
-
-            RuleFor(x => x.user_type_id)
-                .GreaterThan(0).WithMessage("User Type ID must be greater than zero.")
-                .NotEmpty().WithMessage("User Type ID is required.");
-
-            RuleFor(x => x.maker_id)
-                .GreaterThan(0).WithMessage("Maker ID must be greater than zero.")
-                .NotEmpty().WithMessage("Maker ID is required.");
-
-            RuleFor(x => x.make_time)
-                .NotEmpty().WithMessage("Make time is required.");
-
-            RuleFor(x => x.last_modified_by)
-                .GreaterThan(0).WithMessage("Last modified by must be greater than zero.")
-                .NotEmpty().WithMessage("Last modified by is required.");
-
-            RuleFor(x => x.last_modified_date)
-                .NotEmpty().WithMessage("Last modified date is required.");
+            case 1: Console.WriteLine("January"); break;
+            case 2: Console.WriteLine("February"); break;
+            case 3: Console.WriteLine("March"); break;
+            case 4: Console.WriteLine("April"); break;
+            case 5: Console.WriteLine("May"); break;
+            case 6: Console.WriteLine("June"); break;
+            case 7: Console.WriteLine("July"); break;
+            case 8: Console.WriteLine("August"); break;
+            case 9: Console.WriteLine("September"); break;
+            case 10: Console.WriteLine("October"); break;
+            case 11: Console.WriteLine("November"); break;
+            case 12: Console.WriteLine("December"); break;
+            default: Console.WriteLine("Invalid month!"); break;
         }
     }
 }
