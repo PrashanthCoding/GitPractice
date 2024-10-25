32025f524b7e17f4f5307af33eb252cf331e3f1d
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-25 10:48:46 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,23 +1,26 @@
 using System;
 
-class MatrixMultiplication
+class MatrixRotation
 {
     static void Main()
     {
-        int[,] matrix1 = { { 1, 2, 3 }, { 4, 5, 6 } };
-        int[,] matrix2 = { { 7, 8 }, { 9, 10 }, { 11, 12 } };
-        int[,] result = new int[2, 2];
+        int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        int n = matrix.GetLength(0);
+        int[,] rotated = new int[n, n];
 
-        for (int i = 0; i < 2; i++)
+        for (int i = 0; i < n; i++)
         {
-            for (int j = 0; j < 2; j++)
+            for (int j = 0; j < n; j++)
             {
-                result[i, j] = 0;
-                for (int k = 0; k < 3; k++)
-                {
-                    result[i, j] += matrix1[i, k] * matrix2[k, j];
-                }
-                Console.Write(result[i, j] + "\t");
+                rotated[j, n - 1 - i] = matrix[i, j];
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(rotated[i, j] + "\t");
             }
             Console.WriteLine();
         }
