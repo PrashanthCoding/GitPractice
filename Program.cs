7838f15acbc7cf0c199ec4248b2483da698ff2de
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-25 14:34:20 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,30 +1,20 @@
 using System;
 
-class MergeSortedArrays
+class MissingNumber
 {
     static void Main()
     {
-        int[] arr1 = { 1, 3, 5, 7 };
-        int[] arr2 = { 2, 4, 6, 8 };
-        int[] merged = new int[arr1.Length + arr2.Length];
+        int[] arr = { 1, 2, 4, 5, 6 };
+        int n = arr.Length + 1;
+        int totalSum = (n * (n + 1)) / 2;
+        int arrSum = 0;
 
-        int i = 0, j = 0, k = 0;
-
-        while (i < arr1.Length && j < arr2.Length)
+        foreach (int num in arr)
         {
-            merged[k++] = arr1[i] < arr2[j] ? arr1[i++] : arr2[j++];
+            arrSum += num;
         }
 
-        while (i < arr1.Length)
-        {
-            merged[k++] = arr1[i++];
-        }
-
-        while (j < arr2.Length)
-        {
-            merged[k++] = arr2[j++];
-        }
-
-        Console.WriteLine("Merged Array: " + string.Join(", ", merged));
+        int missingNumber = totalSum - arrSum;
+        Console.WriteLine("Missing Number: " + missingNumber);
     }
 }
