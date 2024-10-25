33c9eb0837244b0c89fb8d97f134b3595f575159
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-25 17:10:24 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,1 +1,27 @@
-Hi Prashanth
+using System;
+
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Barking...");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Bark();
+    }
+}
