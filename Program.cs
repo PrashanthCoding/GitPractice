df205b5126e50e51b1957d6e8061f4d682ac9c51
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-25 15:18:33 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,27 +1,22 @@
 using System;
+using System.Collections.Generic;
 
-class TransposeMatrix
+class CommonElements
 {
     static void Main()
     {
-        int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        int[,] transpose = new int[matrix.GetLength(1), matrix.GetLength(0)];
+        int[] array1 = { 1, 2, 3, 4, 5 };
+        int[] array2 = { 4, 5, 6, 7, 8 };
+        List<int> common = new List<int>();
 
-        for (int i = 0; i < matrix.GetLength(0); i++)
+        foreach (int num in array1)
         {
-            for (int j = 0; j < matrix.GetLength(1); j++)
+            if (Array.Exists(array2, element => element == num))
             {
-                transpose[j, i] = matrix[i, j];
+                common.Add(num);
             }
         }
 
-        for (int i = 0; i < transpose.GetLength(0); i++)
-        {
-            for (int j = 0; j < transpose.GetLength(1); j++)
-            {
-                Console.Write(transpose[i, j] + "\t");
-            }
-            Console.WriteLine();
-        }
+        Console.WriteLine("Common Elements: " + string.Join(", ", common));
     }
 }
