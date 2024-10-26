d87cf570e6a77237785d3e5ddf2aec9ce07f65a1
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-26 12:06:07 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,54 +1,27 @@
 using System;
 
-class Vehicle
+class Notification
 {
-    public string Brand { get; set; }
-    public Vehicle(string brand)
-    {
-        Brand = brand;
-    }
-
-    public void DisplayBrand()
-    {
-        Console.WriteLine($"Vehicle Brand: {Brand}");
-    }
+    public virtual void Send() => Console.WriteLine("Sending notification...");
 }
 
-class Car : Vehicle
+class EmailNotification : Notification
 {
-    public int Speed { get; set; }
-    public Car(string brand, int speed) : base(brand)
-    {
-        Speed = speed;
-    }
-
-    public void DisplaySpeed()
-    {
-        Console.WriteLine($"{Brand} car speed: {Speed} km/h");
-    }
+    public override void Send() => Console.WriteLine("Sending email notification...");
 }
 
-class ElectricCar : Car
+class SMSNotification : Notification
 {
-    public int BatteryCapacity { get; set; }
-    public ElectricCar(string brand, int speed, int batteryCapacity) : base(brand, speed)
-    {
-        BatteryCapacity = batteryCapacity;
-    }
-
-    public void DisplayBattery()
-    {
-        Console.WriteLine($"{Brand} has a battery capacity of {BatteryCapacity} kWh.");
-    }
+    public override void Send() => Console.WriteLine("Sending SMS notification...");
 }
 
 class Program
 {
     static void Main()
     {
-        ElectricCar myTesla = new ElectricCar("Tesla", 200, 100);
-        myTesla.DisplayBrand();
-        myTesla.DisplaySpeed();
-        myTesla.DisplayBattery();
+        Notification email = new EmailNotification();
+        Notification sms = new SMSNotification();
+        email.Send();
+        sms.Send();
     }
 }
