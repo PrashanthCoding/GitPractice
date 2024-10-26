60acf5846ba83541ff6234e62463d573d6cfacb8
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-26 11:36:06 +0530

    Class5.cs modified

---
+++ Class5.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class BankAccount
+{
+    public virtual void CalculateInterest() => Console.WriteLine("Calculating interest for bank account.");
+}
+
+class SavingsAccount : BankAccount
+{
+    public override void CalculateInterest() => Console.WriteLine("Interest calculated at 4% for savings account.");
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount acc = new SavingsAccount();
+        acc.CalculateInterest();
+    }
+}
