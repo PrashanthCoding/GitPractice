b3444adee00f3f7b0e3464297195b81dbd433cd1
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-28 11:23:10 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
 
-var number = 10;
-Console.WriteLine(number);
+var dictionary = new Dictionary<string, int> { { "One", 1 }, { "Two", 2 } };
+foreach (var item in dictionary)
+    Console.WriteLine($"{item.Key}: {item.Value}");
