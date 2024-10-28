afe5a6a0a9023d590df417fd34d4456b6ee4d786
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-28 12:44:58 +0530

    Test.cs added

---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = Convert.ToInt32(Console.ReadLine());
+
+        if (num % 2 == 0)
+            Console.WriteLine($"{num} is even.");
+        else
+            Console.WriteLine($"{num} is odd.");
+    }
+}
