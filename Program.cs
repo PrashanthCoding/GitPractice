5f38b7a8a64f8b3ed82d49be837ba62557c9ff97
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-28 10:19:02 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,25 +1,22 @@
 using System;
 
-class Vehicle
+class Address
 {
-    public string Brand { get; set; }
+    public string City { get; set; }
 }
 
-class Car : Vehicle
+class University
 {
-    public int Speed { get; set; }
-}
-
-class ElectricCar : Car
-{
-    public int BatteryCapacity { get; set; }
+    public Address UniversityAddress { get; set; }
+    public University() { UniversityAddress = new Address(); }
 }
 
 class Program
 {
     static void Main()
     {
-        ElectricCar tesla = new ElectricCar { Brand = "Tesla", Speed = 200, BatteryCapacity = 100 };
-        Console.WriteLine($"{tesla.Brand}, Speed: {tesla.Speed}, Battery: {tesla.BatteryCapacity} kWh");
+        University uni = new University();
+        uni.UniversityAddress.City = "New York";
+        Console.WriteLine($"University located in {uni.UniversityAddress.City}");
     }
 }
