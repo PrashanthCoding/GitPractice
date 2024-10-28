8d1e3bad5c69f39ed8457ee63c93381b0a7c464e
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-28 11:20:14 +0530

    Program.cs modified

--- Program.cs
+++ Program.cs
@@ -1,34 +1,4 @@
 using System;
 
-class Calculator
-{
-    public int Add(int a, int b)
-    {
-        return Calculate(a, b, '+');
-    }
-
-    public int Subtract(int a, int b)
-    {
-        return Calculate(a, b, '-');
-    }
-
-    private int Calculate(int a, int b, char operation)
-    {
-        return operation switch
-        {
-            '+' => a + b,
-            '-' => a - b,
-            _ => 0
-        };
-    }
-}
-
-class Program
-{
-    static void Main()
-    {
-        Calculator calc = new Calculator();
-        Console.WriteLine(calc.Add(10, 5));  // Output: 15
-        Console.WriteLine(calc.Subtract(10, 5));  // Output: 5
-    }
-}
+var number = 10;
+Console.WriteLine(number);
