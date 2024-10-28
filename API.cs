1d2444a980800e068d767ac91273f5e8ec3c68d7
PrashanthDevOps <prashanth100076@gmail.com>
2024-10-28 12:47:15 +0530

    API class added

---
+++ API.cs
@@ -0,0 +1,29 @@
+﻿[ApiController]
+[Route("api/[controller]")]
+public class CalculatorController : ControllerBase
+{
+    [HttpGet("add/{a}/{b}")]
+    public IActionResult Add(int a, int b)
+    {
+        return Ok(a + b);
+    }
+
+    [HttpGet("subtract/{a}/{b}")]
+    public IActionResult Subtract(int a, int b)
+    {
+        return Ok(a - b);
+    }
+
+    [HttpGet("multiply/{a}/{b}")]
+    public IActionResult Multiply(int a, int b)
+    {
+        return Ok(a * b);
+    }
+
+    [HttpGet("divide/{a}/{b}")]
+    public IActionResult Divide(int a, int b)
+    {
+        if (b == 0) return BadRequest("Division by zero is not allowed.");
+        return Ok((double)a / b);
+    }
+}
